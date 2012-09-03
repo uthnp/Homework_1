ac819e749bd5284a866afbb2ca51c7cb92574fe0
Nicholas Uth <uthnp@muohio.edu>
2012-09-03 14:16:15 -0400

    finished(?) rectangle method, started line method

--- src/Homework_1App.cpp
+++ src/Homework_1App.cpp
@@ -29,9 +29,19 @@ class Homework_1App : public AppBasic {
   private:
 	Surface* mySurface_;
 
+	/**
+	* Draws a rectangle of the specified color.
+	*
+	* Changes the color value of the pixels representing the rectangle with it's top left corner at
+	* (topLeftX, topLeftY) as well as a width and height.
+	*
+	* Satisfies goal A.1
+	*/
 	void drawRectangle (uint8_t* pixels, int topLeftX, int topLeftY, int width, int height, Color8u color);
+
+
 	void drawCircle (uint8_t* pixels, int topLeftX, int topLeftY, int radius, Color8u color);
-	void drawLine (uint8_t* pixels, int point1X, int point1Y, int point2X, int point2Y);
+	void drawLine (uint8_t* pixels, int point1X, int point1Y, int point2X, int point2Y, Color8u color);
 	void drawTriangle (uint8_t* pixels, int point1X, int point1Y, int point2X, int point2Y, int point3X, int point3Y, Color8u color);
 
 	//Width and height of the screen
@@ -49,7 +59,6 @@ void Homework_1App::setup()
 {
 	mySurface_ = new Surface(kTextureSize,kTextureSize,false);
 
-
 }
 
 void Homework_1App::mouseDown( MouseEvent event )
@@ -58,14 +67,39 @@ void Homework_1App::mouseDown( MouseEvent event )
 
 void Homework_1App::drawRectangle (uint8_t* pixels, int topLeftX, int topLeftY, int width, int height, Color8u color)
 {
+	int x;
+	int y;
+
+	// run through the right coordinates for the rectangle, then use them to change the color value of the appropriate pixels
+	for (y = topLeftY; y < (topLeftY + height); y++)
+	{
+		for (x = topLeftX; y < (topLeftX + width); x++)
+		{
+			pixels[3*(x + y*kTextureSize)] = color.r;
+			pixels[3*(x + y*kTextureSize)+1] = color.g;
+			pixels[3*(x + y*kTextureSize)+2] = color.b;
+		}
+	}
 }
 
 void Homework_1App::drawCircle (uint8_t* pixels, int topLeftX, int topLeftY, int radius, Color8u color)
 {
 }
 
-void Homework_1App::drawLine (uint8_t* pixels, int point1X, int point1Y, int point2X, int point2Y)
+void Homework_1App::drawLine (uint8_t* pixels, int point1X, int point1Y, int point2X, int point2Y, Color8u color)
 {
+	//based on y = mx+b
+	int slope = ((point1Y - point2Y)/(point1X - point2X));
+	int intersect = (point1Y - (slope*point1X));
+	int xVariable;
+	int yVariable;
+
+	if (point1X == point2X)
+	{
+
+		return;
+	}
+	
 }
 
 void Homework_1App::drawTriangle (uint8_t* pixels, int point1X, int point1Y, int point2X, int point2Y, int point3X, int point3Y, Color8u color)
