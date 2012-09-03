58ec5b4c3e9e5d9d15e1c816267d641d3ef9a13e
Nicholas Uth <uthnp@muohio.edu>
2012-09-03 15:35:21 -0400

    Finished(?) the drawCircle method

--- src/Homework_1App.cpp
+++ src/Homework_1App.cpp
@@ -3,7 +3,9 @@
 *	Program written to produce an image of some cards using basic shapes.
 *	Written for CSE 274 on Sept 3, 2012.
 *	
-*	@Note: some lines of code are taken from: https://github.com/brinkmwj/HW01
+*	@Note: some misc lines of code are taken from: https://github.com/brinkmwj/HW01
+*	@Note: the midpoint algorithm used for the drawCircle method is modified from the one found here:
+*		   http://en.wikipedia.org/wiki/Midpoint_circle_algorithm
 *
 *	@Note: program satisfies the following project requirements:
 *		A.1, A.2, A.3, A.7
@@ -45,13 +47,15 @@ class Homework_1App : public AppBasic {
 	*
 	* Satisfies goal A.2
 	*/
-	void drawCircle (uint8_t* pixels, int topLeftX, int topLeftY, int radius, Color8u color);
+	void drawCircle (uint8_t* pixels, int centerX, int centerY, int radius, Color8u color);
 
 	/**
 	* Draws a line of the specified color.
 	*
 	* Changes the color of the pixels representing the line from (point1X, point1Y) and (point2X, point2Y) to the
 	* specified color.
+	*
+	* TODO: could stand to be optimized in terms of code length... just kind of brute forced it to be safe
 	*
 	* Satisfies goal A.3
 	*/
@@ -91,9 +95,9 @@ void Homework_1App::drawRectangle (uint8_t* pixels, int topLeftX, int topLeftY, 
 	int y;
 
 	// run through the right coordinates for the rectangle, then use them to change the color value of the appropriate pixels
-	for (y = topLeftY; y < (topLeftY + height); y++)
+	for (y = topLeftY; y <= (topLeftY + height); y++)
 	{
-		for (x = topLeftX; y < (topLeftX + width); x++)
+		for (x = topLeftX; x <= (topLeftX + width); x++)
 		{
 			pixels[3*(x + y*kTextureSize)] = color.r;
 			pixels[3*(x + y*kTextureSize)+1] = color.g;
@@ -102,8 +106,35 @@ void Homework_1App::drawRectangle (uint8_t* pixels, int topLeftX, int topLeftY, 
 	}
 }
 
-void Homework_1App::drawCircle (uint8_t* pixels, int topLeftX, int topLeftY, int radius, Color8u color)
+void Homework_1App::drawCircle (uint8_t* pixels, int centerX, int centerY, int radius, Color8u color)
 {
+	// using modified version of the midpoint formula method from here: http://en.wikipedia.org/wiki/Midpoint_circle_algorithm
+	// also... yes, I am using wikipedia as a source. In this case, if it works then what does it matter?
+
+	int f = 1-radius;
+	int ddF_x = 1;
+	int ddF_y = -2 * radius;
+	int x = 0;
+	int y = radius;
+	//handle the points along the horiz and vert that intersect the midpoint
+	drawLine(pixels, centerX, (centerY-radius), centerX, (centerY+radius), color);
+	drawLine(pixels, (centerX-radius), centerY, (centerX+radius), centerY, color);
+	while (x < y)
+	{
+		if (f >= 0)
+		{
+			y--;
+			ddF_y += 2;
+			f += ddF_y;
+		}
+		x++;
+		ddF_x += 2;
+		f += ddF_x;
+		drawLine (pixels, (centerX + x), (centerY + y), (centerX - x), (centerY + y),color);
+		drawLine (pixels, (centerX + x), (centerY - y), (centerX - x), (centerY - y),color);
+		drawLine (pixels, (centerX + y), (centerY + x), (centerX - y), (centerY + x),color);
+		drawLine (pixels, (centerX + y), (centerY - x), (centerX - y), (centerY - x),color);
+	}
 }
 
 void Homework_1App::drawLine (uint8_t* pixels, int point1X, int point1Y, int point2X, int point2Y, Color8u color)
@@ -117,7 +148,7 @@ void Homework_1App::drawLine (uint8_t* pixels, int point1X, int point1Y, int poi
 	if (point1X == point2X) //to prevent any div by 0 errors
 	{
 		x = point1X;
-		for (y = min(point1Y, point2Y); y < max(point1Y, point2Y); y++)
+		for (y = min(point1Y, point2Y); y <= max(point1Y, point2Y); y++)
 		{
 			pixels[3*(x + y*kTextureSize)] = color.r;
 			pixels[3*(x + y*kTextureSize)+1] = color.g;
@@ -130,9 +161,9 @@ void Homework_1App::drawLine (uint8_t* pixels, int point1X, int point1Y, int poi
 	intersect = (point1Y - (slope*point1X));
 	if (slope > 0)
 	{
-		for (y = point1Y; y < point2Y; y++)
+		for (y = point1Y; y <= point2Y; y++)
 		{
-			for (x = point1X; x < point2X; x++)
+			for (x = point1X; x <= point2X; x++)
 			{
 				pixels[3*(x + y*kTextureSize)] = color.r;
 				pixels[3*(x + y*kTextureSize)+1] = color.g;
@@ -142,9 +173,9 @@ void Homework_1App::drawLine (uint8_t* pixels, int point1X, int point1Y, int poi
 	}
 	else if (slope < 0)
 	{
-		for (y = point2Y; y < point1Y; y++)
+		for (y = point2Y; y <= point1Y; y++)
 		{
-			for (x = point2X; x < point1X; x++)
+			for (x = point2X; x <= point1X; x++)
 			{
 				pixels[3*(x + y*kTextureSize)] = color.r;
 				pixels[3*(x + y*kTextureSize)+1] = color.g;
@@ -154,7 +185,7 @@ void Homework_1App::drawLine (uint8_t* pixels, int point1X, int point1Y, int poi
 	}
 	else // if slope == 0
 	{
-		for (x = min(point1X, point2X); x < max(point1X, point2X); x++)
+		for (x = min(point1X, point2X); x <= max(point1X, point2X); x++)
 		{
 			y = point1Y;
 			pixels[3*(x + y*kTextureSize)] = color.r;
