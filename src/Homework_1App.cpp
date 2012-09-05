f81a303b47148fd4279858b104f910573d6e2b6e
Nicholas Uth <uthnp@muohio.edu>
2012-09-04 20:51:53 -0400

    Fixed up line method... it finally works right
    wrote my own code based on Bresenham's Line Algorithm to fix it

--- src/Homework_1App.cpp
+++ src/Homework_1App.cpp
@@ -59,9 +59,9 @@ class Homework_1App : public AppBasic {
 	* Changes the color of the pixels representing the line from (point1X, point1Y) and (point2X, point2Y) to the
 	* specified color.
 	*
-	* TODO: could stand to be optimized in terms of code length... just kind of brute forced it to be safe
-	* TODO: creating rectangles for diagonal lines... need to fix that...
-	* TODO: lines with positive slopes are not appearing at all for some reason.
+	* @Note: Based on the Bresenham's Line Algorithm. I didn't use a reference or source code though.
+	*		 I wrote what is in here based on my analysis of the theory. It may, however, be unintentionally similar
+	*        to preexisting code. 
 	*
 	* Satisfies goal A.3
 	*/
@@ -148,58 +148,49 @@ void Homework_1App::drawCircle (uint8_t* pixels, int centerX, int centerY, int r
 
 void Homework_1App::drawLine (uint8_t* pixels, int point1X, int point1Y, int point2X, int point2Y, Color8u color)
 {
-	//based on y = mx+b
-	int slope;
-	int intersect;
+	// The following is based on the theory behind Bresenham's Line algorithm
 	int x;
 	int y;
+	double dx = (point1X - point2X);
+	double dy = (point1Y - point2Y);
+	double percent;
 
-	if (point1X == point2X) //to prevent any div by 0 errors
+	if (abs(dx) > abs(dy))
 	{
-		x = point1X;
-		for (y = min(point1Y, point2Y); y <= max(point1Y, point2Y); y++)
+		for (int i = min(point1X,point2X); i <= max(point1X, point2X); i++)
 		{
-			pixels[3*(x + y*kTextureSize)] = color.r;
-			pixels[3*(x + y*kTextureSize)+1] = color.g;
-			pixels[3*(x + y*kTextureSize)+2] = color.b;
-		}
-		return;
-	}
-	
-	slope = ((point1Y - point2Y)/(point1X - point2X));
-	intersect = (point1Y - (slope*point1X));
-	if (slope > 0)
-	{
-		for (y = point1Y; y <= point2Y; y++)
-		{
-			for (x = point1X; x <= point2X; x++)
+			percent = (i - min(point1X,point2X))/dx;
+			if (min(point1X, point2X) == point1X) // to make sure the X and Y coords stay synched
 			{
-				pixels[3*(x + y*kTextureSize)] = color.r;
-				pixels[3*(x + y*kTextureSize)+1] = color.g;
-				pixels[3*(x + y*kTextureSize)+2] = color.b;
+				y = point1Y + (percent * dy);
 			}
-		}
-	}
-	else if (slope < 0)
-	{
-		for (y = point2Y; y <= point1Y; y++)
-		{
-			for (x = point2X; x <= point1X; x++)
+			else
 			{
-				pixels[3*(x + y*kTextureSize)] = color.r;
-				pixels[3*(x + y*kTextureSize)+1] = color.g;
-				pixels[3*(x + y*kTextureSize)+2] = color.b;
+				y = point2Y + (percent * dy);
 			}
+			// in this case... i = x
+			pixels[3*(i + y*kTextureSize)] = color.r;
+			pixels[3*(i + y*kTextureSize)+1] = color.g;
+			pixels[3*(i + y*kTextureSize)+2] = color.b;
 		}
 	}
-	else // if slope == 0
+	else // more y axis movement
 	{
-		for (x = min(point1X, point2X); x <= max(point1X, point2X); x++)
+		for (int i = min(point1Y,point2Y); i <= max(point1Y, point2Y); i++)
 		{
-			y = point1Y;
-			pixels[3*(x + y*kTextureSize)] = color.r;
-			pixels[3*(x + y*kTextureSize)+1] = color.g;
-			pixels[3*(x + y*kTextureSize)+2] = color.b;
+			percent = (i - min(point1Y,point2Y))/dy;
+			if (min(point1Y, point2Y) == point1Y) // to make sure the X and Y coords stay synched
+			{
+				x = point1X + (percent * dx);
+			}
+			else
+			{
+				x = point2X + (percent * dx);
+			}
+			// in this case... i = y
+			pixels[3*(x + i*kTextureSize)] = color.r;
+			pixels[3*(x + i*kTextureSize)+1] = color.g;
+			pixels[3*(x + i*kTextureSize)+2] = color.b;
 		}
 	}
 }
@@ -232,13 +223,30 @@ void Homework_1App::update()
 {
 	//test
 	uint8_t* pixels = (*mySurface_).getData();
-	Color8u color = Color8u(150, 0, 0);
+	//Color8u backColor = Color8u (10, 200, 10);
+	Color8u backColor = Color8u (0, 0, 0);
+	
+	int numPixels = max(kAppWidth, kAppHeight);
+	numPixels *= numPixels;
+
+	for (int i = 0; i < (3*numPixels); i += 3)
+	{
+		pixels[i] = backColor.r;
+		pixels[i+1] = backColor.g;
+		pixels[i+2] = backColor.b;
+	}
+
+	Color8u color1 = Color8u(150, 0, 0);
+	Color8u color2 = Color8u(0, 150, 0);
+	Color8u color3 = Color8u(0, 0, 150);
+	Color8u color4 = Color8u(100, 100, 0);
 	//drawRectangle(pixels, 50, 50, 60, 30, color);
 	//drawCircle (pixels, 150, 150, 70, color);
-	//drawLine (pixels, 200, 0, 200, 300, color);
-	//drawLine (pixels, 200, 100, 0, 100, color);
-	//drawLine (pixels, 200, 0, 400, 300, color);
-	drawTriangle (pixels, 100, 400, 300, 400, 100, 100, color);
+	drawLine (pixels, 100, 300, 300, 300, color1); // m = 0
+	drawLine (pixels, 200, 200, 200, 400, color2); // m = oo
+	drawLine (pixels, 200, 200, 600, 500, color3); // m = +
+	drawLine (pixels, 350, 350, 500, 100, color4); // m = -
+	//drawTriangle (pixels, 100, 400, 300, 400, 100, 100, color);
 }
 
 void Homework_1App::draw()
